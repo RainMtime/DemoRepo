29700135e8b17dfe00bce59ab6da21b59386d4b9
chunyu <746431278@qq.com>
2018-11-19 17:24:22 +0800

    提交可拖动recyclerViewItem的实现代码

---
+++ app/src/main/java/rainmtime/com/demorepo/component/view/drag_recyclerview/ItemTouchHelperAdapter.java
@@ -0,0 +1,8 @@
+package rainmtime.com.demorepo.component.view.drag_recyclerview;
+
+/**
+ * Created by chunyu on 2018/11/19 5:13 PM.
+ * Email: 746431278@qq.com
+ */
+public interface ItemTouchHelperAdapter {
+}
---
+++ app/src/main/java/rainmtime/com/demorepo/component/view/drag_recyclerview/ItemTouchHelperCallback.java
@@ -0,0 +1,8 @@
+package rainmtime.com.demorepo.component.view.drag_recyclerview;
+
+/**
+ * Created by chunyu on 2018/11/19 4:44 PM.
+ * Email: 746431278@qq.com
+ */
+public class ItemTouchHelperCallback {
+}
