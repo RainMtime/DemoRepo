e662a55c10db19c14d648476537d5e20189ad826
chunyujin <chunyujin@tencent.com>
2019-05-30 20:32:34 +0800

    提交okhttp demo 测试代码。 修改gralde 中最小版本的支持。

--- app/src/main/java/rainmtime/com/demorepo/test_code/okhttp/OkHttpActivity.java
+++ app/src/main/java/rainmtime/com/demorepo/test_code/okhttp/OkHttpActivity.java
@@ -6,10 +6,15 @@ import android.support.v7.app.AppCompatActivity;
 
 import java.io.IOException;
 
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.Headers;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 /**
  * Created by 人间一小雨 on 2019-05-28 22:17
@@ -53,6 +58,60 @@ public class OkHttpActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void asynchronousGet(){
+        Request request = new Request.Builder()
+                .url("http://publicobject.com/helloworld.txt")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override public void onResponse(Call call, Response response) throws IOException {
+                try (ResponseBody responseBody = response.body()) {
+                    if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+                    Headers responseHeaders = response.headers();
+                    for (int i = 0, size = responseHeaders.size(); i < size; i++) {
+                        System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
+                    }
+
+                    System.out.println(responseBody.string());
+                }
+            }
+        });
+    }
+
+    public void postString () {
+         final MediaType MEDIA_TYPE_MARKDOWN
+                = MediaType.parse("text/x-markdown; charset=utf-8");
+
+
+
+            String postBody = ""
+                    + "Releases\n"
+                    + "--------\n"
+                    + "\n"
+                    + " * _1.0_ May 6, 2013\n"
+                    + " * _1.1_ June 15, 2013\n"
+                    + " * _1.2_ August 11, 2013\n";
+
+            Request request = new Request.Builder()
+                    .url("https://api.github.com/markdown/raw")
+                    .post(RequestBody.create(MEDIA_TYPE_MARKDOWN, postBody))
+                    .build();
+
+            try (Response response = client.newCall(request).execute()) {
+                if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+                System.out.println(response.body().string());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
     }
 
 }
